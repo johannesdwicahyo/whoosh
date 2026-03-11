aab9b0e52a952174d5b2d794db9c9ad4eb39d278
johannesdwicahyo <csk.rage@gmail.com>
2026-03-11 23:00:07 +0700

    feat: add Response with JSON serialization and error formatting

---
+++ lib/whoosh/response.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Whoosh
+  class Response
+    def self.json(data, status: 200, headers: {})
+      body = Serialization::Json.encode(data)
+      response_headers = {
+        "content-type" => "application/json",
+        "content-length" => body.bytesize.to_s
+      }.merge(headers)
+
+      [status, response_headers, [body]]
+    end
+
+    def self.error(error, production: false)
+      headers = { "content-type" => "application/json" }
+
+      if error.is_a?(Errors::RateLimitExceeded)
+        headers["retry-after"] = error.retry_after.to_s
+      end
+
+      body = if error.is_a?(Errors::HttpError)
+        error.to_h
+      else
+        hash = { error: "internal_error" }
+        hash[:message] = error.message unless production
+        hash
+      end
+
+      [error.is_a?(Errors::HttpError) ? error.status : 500, headers, [Serialization::Json.encode(body)]]
+    end
+
+    def self.not_found
+      error(Errors::NotFoundError.new)
+    end
+  end
+end
