1b48be9e40d89225c77d71a4fbe62da825842ff7
johannesdwicahyo <csk.rage@gmail.com>
2026-03-16 12:18:33 +0700

    feat: add MCP client subprocess spawning with Open3 and PID tracking

--- lib/whoosh/mcp/client_manager.rb
+++ lib/whoosh/mcp/client_manager.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require "open3"
+require "json"
+
 module Whoosh
   module MCP
     class ClientManager
       def initialize
         @configs = {}
         @clients = {}
+        @pids = {}
         @mutex = Mutex.new
       end
 
@@ -29,10 +33,39 @@ module Whoosh
         @mutex.synchronize { @clients[name] }
       end
 
+      def spawn_client(name)
+        config = @configs[name]
+        raise Whoosh::Errors::DependencyError, "Unknown MCP client: #{name}" unless config
+
+        stdin, stdout, stderr, wait_thr = Open3.popen3(config[:command])
+
+        client = Client.new(stdin: stdin, stdout: stdout)
+
+        @mutex.synchronize do
+          @clients[name] = client
+          @pids[name] = { pid: wait_thr.pid, thread: wait_thr, stderr: stderr }
+        end
+
+        client
+      end
+
+      def pids
+        @mutex.synchronize { @pids.transform_values { |v| v[:pid] } }
+      end
+
       def shutdown_all
         @mutex.synchronize do
           @clients.each_value { |c| c.close if c.respond_to?(:close) }
+          @pids.each_value do |info|
+            begin
+              Process.kill("TERM", info[:pid])
+              info[:thread]&.join(5)
+            rescue Errno::ESRCH, Errno::ECHILD
+            end
+            info[:stderr]&.close rescue nil
+          end
           @clients.clear
+          @pids.clear
         end
       end
     end
